7913f4e24784c0db2e795f5df13bfaad0451a251
He Qingbao <qingbao.ho@gmail.com>
2018-05-06 02:13:57 +0800

    User email confirmation

--- src/models/User.js
+++ src/models/User.js
@@ -12,7 +12,8 @@ const schema = new mongoose.Schema({
         unique: true
     },
     passwordHash: {type: String, request: true},
-    confirmed: {type: Boolean, default: false}
+    confirmed: {type: Boolean, default: false},
+    confirmationToken: {type: String, default: ""}
 }, {timestamps: true});
 
 schema.methods.isValidPassword = function isValidPassword(password) {
@@ -23,9 +24,18 @@ schema.methods.setPassword = function setPassword(password) {
     this.passwordHash = bcrypt.hashSync(password, 10);
 };
 
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+    this.confirmationToken = this.generateJWT();
+};
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+    return `${process.env.HOST}/confirmation/${this.confirmationToken}`
+}
+
 schema.methods.generateJWT = function generateJWT() {
     return jwt.sign({
         email: this.email,
+        confirmed: this.confirmed
     }, process.env.JWT_SECRET)
 };
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import User from '../models/User';
 import parseErrors from "../utils/parseErrors";
+import {sendConfirmationEmail} from '../mailer';
 
 const router = express.Router();
 
@@ -9,9 +10,17 @@ router.post('/', (req, res) => {
     const {email, password} = req.body.user;
     const user = new User({email});
     user.setPassword(password);
+    user.setConfirmationToken();
     user.save()
-        .then(userRecord => res.json({user: userRecord.toAuthJSON()}))
-        .catch(err => res.status(400).json({errors: parseErrors(err.errors)}));
+        .then(userRecord => {
+            console.log("send mail...");
+            sendConfirmationEmail(userRecord);
+            res.json({user: userRecord.toAuthJSON()});
+        })
+        .catch(err => {
+            console.log("error");
+            res.status(400).json({errors: parseErrors(err.errors)})
+        });
 });
 
 export default router;
